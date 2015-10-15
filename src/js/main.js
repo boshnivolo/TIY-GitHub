67aa851d69a57656b3b0ab60d363896903e2c7be
Jasmine Baik <boshnivolo@gmail.com>
2015-10-14 23:30:24 -0400

    Cause tabs to move...not what I want

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,10 @@
 // For now make the tab 'repo' always active.
 
 $(' a[href="#tab1"]').on('click', function (){
-  console.log('tracer 1');
   $(' section#tab1').toggleClass('active').siblings().removeClass('active');
 });
 
 $(' a[href="#tab2"]').on('click', function (){
-  console.log('tracer 2');
   $(' section#tab2').toggleClass('active').siblings().removeClass('active');
 });
 
